909448836c4e47ece1118ef574700bfc3af33691
JaamoK <jarno.kankainen1@gmail.com>
2025-05-20 11:38:23 +0300

    Controller

--- Assets/Scripts/Simple3DPlayer.cs
+++ Assets/Scripts/Simple3DPlayer.cs
@@ -1,18 +1,60 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class Simple3DPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed = 5f;
+    public float lookSpeed = 2f;
+    public float jumpHeight = 1.5f;
+    public float gravity = -9.81f;
+
+    private CharacterController controller;
+    private Vector3 velocity;
+    private bool isGrounded;
+
+    public Transform cameraTransform;
+    private float pitch = 0f;
+
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        Look();
+    }
+
+    void Move()
+    {
+        isGrounded = controller.isGrounded;
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * moveSpeed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    void Look()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * lookSpeed;
+        float mouseY = Input.GetAxis("Mouse Y") * lookSpeed;
+
+        pitch -= mouseY;
+        pitch = Mathf.Clamp(pitch, -75f, 75f);
+
+        cameraTransform.localRotation = Quaternion.Euler(pitch, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
     }
 }
